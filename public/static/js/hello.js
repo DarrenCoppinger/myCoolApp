6d3c52cdff757eaad8e2159434ca3f77f5daebb2
DarrenCoppinger <darrencoppinger@gmail.com>
2021-10-06 11:19:27 +0100

    add javascript prompts

--- public/static/js/hello.js
+++ public/static/js/hello.js
@@ -1,2 +1,39 @@
-window.alert(“Alert”);
+var salary = parseInt(
+    prompt("Please enter your salary","Type here")
+);
 
+var statement;
+if(salary > 500000){
+   statement = "You are wealthy!";
+}else if(salary < 100000){
+    statement = "You are poor!";
+} else {
+    statement = "You are average!";
+}
+
+alert(statement);
+
+//
+// var Monexpenses = parseInt(
+//     prompt("Please enter your monthly expenses","Type")
+// );
+// var AnnualExpenses = Monexpenses*12;
+//
+// var netIncome = salary- AnnualExpenses;
+//
+// var maxBorrow = netIncome*3.5;
+//
+// alert("Max Mortgage Amount "+maxBorrow);
+
+// var age = 20;
+// alert(age);
+
+//
+// var A = 20;
+// var B = 20;
+// var C = 20;
+// var D = 20;
+//
+// var result = A*B+C*D
+//
+// alert(result);
